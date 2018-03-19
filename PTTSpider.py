9d8484b055843e1e13d0859ea8f76c420174bac0
Weian <d92434101@gmail.com>
2018-03-20 07:20:19 +0800

    get lists from the board

--- PTTSpider.py
+++ PTTSpider.py
@@ -8,9 +8,13 @@ class PTTSpider(object):
     def __init__(self):
         self.PTT_URL = "https://www.ptt.cc"
         self.PTT_INDEX_URL = "https://www.ptt.cc/bbs/index.html"
+        self.__boards = None
 
     def get_boards(self):
         text = self.__get_page_source(self.PTT_INDEX_URL)
+        if text is None:
+            return None
+
         soup = BeautifulSoup(text, 'lxml')
         boards = soup.find_all('div', 'b-ent')
         for board in boards:
@@ -27,22 +31,47 @@ class PTTSpider(object):
                 'url': self.PTT_URL + board_url['href']
             }
 
-    def get_lists(self, board):
-        pass
+    def get_lists(self, board_url):
+        text = self.__get_page_source(board_url)
+        soup = BeautifulSoup(text, 'lxml')
+        titles = soup.find_all('div', 'r-ent')
+        for t in titles:
+            title = t.find('div', "title")
+            hot_level = t.find('div', 'nrec')
+            url = t.find('a', href=True)
+            yield {
+                "title": title.text.strip(),
+                "hot_level": str(0 if len(hot_level.text) == 0 else hot_level.text),
+                "url": "" if url is None else url['href']
+            }
 
     def get_content(self, index):
         pass
 
     def __get_page_source(self, url):
-        if not self.__is_ptt_alive():
+        if not self.is_ptt_alive():
             return None
         response = requests.get(url)
         if response.status_code == 200:
             return response.text
         return None
 
-    def __is_ptt_alive(self):
+    def is_ptt_alive(self):
         response = requests.get(self.PTT_URL)
         if response.status_code == 200:
             return True
         return False
+
+
+def main():
+    ptt = PTTSpider()
+    if not ptt.is_ptt_alive():
+        print("ptt is off line")
+        return
+
+    for board in ptt.get_boards():
+        print(board)
+
+
+if __name__ == '__main__':
+    main()
