be2d760698fa536a3a0e6cbef99932219761196f
Weian <d92434101@gmail.com>
2018-04-22 00:35:39 +0800

    1. gather all files about instagram 2. add get album list

---
+++ instagram/InstagramConstants.py
@@ -0,0 +1,4 @@
+WEB_URL = 'https://www.instagram.com'
+PHOTO_NEXT_BUTTON = 'coreSpriteRightChevron'
+PHOTO_PREV_BUTTON = 'coreSpriteLeftChevron'
+USER_TAKEN = '?taken-by='
--- instagram/InstagramSpider.py
+++ instagram/InstagramSpider.py
@@ -1,32 +1,31 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import re
+from instagram.InstagramConstants import WEB_URL, USER_TAKEN, PHOTO_NEXT_BUTTON, PHOTO_PREV_BUTTON
 
 
 INSTAGRAM_USER = "annehathaway"
-INSTAGRAM = "https://www.instagram.com"
-
-GROUP_TAG = '_6d3hm _mnav9'
-PHOTO_TAG = '_mck9w _gvoze  _tn0ps'
-
-ALBUM = '/p/BhNP6DNFwi-'
 
 
 class InstagramSpider(object):
-    def __init__(self):
+    def __init__(self, user):
         self.__current_page_content = ''
         self.__web_browser = webdriver.PhantomJS()
+        self.__instagram_user = user
+
+        # properties
+        self.__date = '19110101'
 
     def get_list(self):
         browser = webdriver.PhantomJS()
-        browser.get(INSTAGRAM + '/' + INSTAGRAM_USER)
+        browser.get(WEB_URL + '/' + INSTAGRAM_USER)
 
-        soup = BeautifulSoup(browser.page_source, 'lxml')
-        groups = soup.find_all('div', GROUP_TAG)
-        for group in groups:
-            albums = group.find_all('a', href=True)
-            for album in albums:
-                print("url: " + INSTAGRAM + album['href'] + '?taken-by=' + INSTAGRAM_USER)
+        albums = re.findall("<a href=\"(/p[\/\d\w]+)\/", browser.page_source)
+        if len(albums) == 0:
+            return None
+
+        for album in albums:
+            yield WEB_URL + album + USER_TAKEN + INSTAGRAM_USER
 
     def get_photos(self, url):
         pass
@@ -40,7 +39,7 @@ class InstagramSpider(object):
         for result in results:
             r = re.search("([_\d\w]+)\s+([_\d\w]+)\s+coreSpriteRightChevron", str(result))
             if r:
-                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.coreSpriteRightChevron'
+                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.' + PHOTO_NEXT_BUTTON
                 self.__web_browser.find_element_by_css_selector(next_button).click()
                 return True
         return False
@@ -52,18 +51,20 @@ class InstagramSpider(object):
         soup = BeautifulSoup(self.__current_page_content, 'lxml')
         results = soup.find_all('div')
         for result in results:
-            r = re.search("([_\d\w]+)\s+([_\d\w]+)\s+coreSpriteLeftChevron", str(result))
+            r = re.search("([_\d\w]+)\s+([_\d\w]+)\s" + PHOTO_PREV_BUTTON, str(result))
             if r:
-                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.coreSpriteLeftChevron'
-                self.__web_browser.find_element_by_css_selector(next_button).click()
+                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.' + PHOTO_PREV_BUTTON
+                self.__web_browser.find_element_by_css_selector(next_button)
                 return True
         return False
 
+    @property
+    def date(self):
+        return self.__date
 
-def main():
-    instagram = InstagramSpider()
-    instagram.get_photos(ALBUM)
+    @date.setter
+    def date(self, date):
+        self.__date = date
 
-
-if __name__ == '__main__':
-    main()
+    def run(self):
+        pass
