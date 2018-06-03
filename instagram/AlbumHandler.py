c05419a14449a907e4e9be849100155a8fdc0769
Weian <d92434101@gmail.com>
2018-06-03 10:37:39 +0800

    refactor AlbumHandler by requests lib

--- instagram/AlbumHandler.py
+++ instagram/AlbumHandler.py
@@ -1,125 +1,79 @@
 import re
 import time
+import requests
+import json
 
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
-from instagram.InstagramConstants import PHOTO_NEXT_BUTTON, PHOTO_PREV_BUTTON
 
 ALBUM_URL = 'https://www.instagram.com/p/BgURvYOlVSR/?taken-by=annehathaway'
 USER = 'annehathaway'
 
 
-class AlbumHandler(object):
+class AlbumHandler2:
     def __init__(self, album_url, user):
         self.__album_url = album_url
-        self.__instagram_user = user
-        self.__likes = -1
-        self.__reply_count = 0
-        self.__date = '1911/1/1'
-        self.__photos = []
-        self.__tags = []
-        # NOTE: For running PhantomJS well, you need to install the PhantomJS from http://phantomjs.org
-        # Set the path direct to the PhantomJS folder as like above. Then you can work well! 😉
-        # self.__browser = webdriver.PhantomJS(executable_path=PATH_OF_PhantomJS)
-        self.__browser = webdriver.PhantomJS()
-        self.__browser.get(self.__album_url)
+        self.user = user
+        self.status_code = 0
+        self.__metadata = {}
 
-    def get_likes(self):
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        result = soup.find_all('span')
-        for r in result:
-            find = re.search('<span>([\d\,]+)</span>\slikes</span>', str(r))
-            if find:
-                self.__likes = int(find.group(1).replace(',', ''))
-                return True
-        return False
+    def run(self):
+        response = requests.get(self.__album_url)
+        self.status_code = response.status_code
+        if response.status_code != 200:
+            return
 
-    def get_date(self):
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        result = soup.find_all('time')
-        for r in result:
-            find = re.match("([\d\w:-]+)\.\d+\w", r['datetime'])
-            if find:
-                time.strftime(time.strptime(find.group(1), "%Y-%m-%dT%H:%M:%S"), "%Y/%m/%d")
-                return True
-        return False
+        soup = BeautifulSoup(response.content.decode(), 'lxml')
+        results = soup.find_all('script', type='text/javascript')
+        for result in results:
+            r = re.search("window._sharedData =", str(result))
+            if r:
+                json_part = result.string
+                json_part = json_part[json_part.find('=') + 2: -1]
+                print(json_part)
+                self.__metadata = json.loads(json_part)
+                break
 
     def get_photos(self):
-        while True:
-            soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-            imgs = soup.find_all('img', alt=True)
-            for img in imgs:
-                self.__photos.append(img['src'])
+        if len(self.__metadata) == 0:
+            return {}
 
-            if not self.next_button():
-                break
-        return len(self.__photos)
+        photos_list = {}
+        for node in self.__metadata['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges']:
+            photos_list[node['node']['id']] = node['node']['display_resources'][2]['src']
+
+        return photos_list
 
     def get_tags(self):
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        results = soup.find_all('a', href=True)
-        for result in results:
-            r = re.search("/explore/tags/", str(result))
-            if r:
-                self.__tags.append(result.text)
-        return len(self.__tags)
+        pass
 
-    def get_reply_count(self):
-        self.load_comments()
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        replies = soup.find_all('a', role=False, title=self.__instagram_user)
-        reply_class = ''
-        for reply in replies:
-            if re.search('li', str(reply.parent)):
-                reply_class = str(reply.parent['class'])
+    def get_comment_count(self):
+        if len(self.__metadata) == 0:
+            return -1
 
-        replies = soup.find_all('li')
-        for reply in replies:
-            if str(reply['class']) == reply_class:
-                self.__reply_count += 1
+        return int(self.__metadata['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_comment']['count'])
 
-    def load_comments(self):
-        # TODO: not finish yet, loading comments only once...
-        while True:
-            soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-            button = soup.find('a', role=True, disabled=False, text='Load more comments')
-            if not button:
-                break
-            self.__browser.find_element_by_css_selector('a.' + '.'.join(button['class'])).click()
+    def get_like_count(self):
+        if len(self.__metadata) == 0:
+            return -1
 
-    def next_button(self):
-        if len(self.__browser.page_source) == 0:
-            return False
+        return int(self.__metadata['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_preview_like']['count'])
 
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        results = soup.find_all('div')
-        for result in results:
-            r = re.search("([_\d\w]+)\s+([_\d\w]+)\s+coreSpriteRightChevron", str(result))
-            if r:
-                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.' + PHOTO_NEXT_BUTTON
-                self.__browser.find_element_by_css_selector(next_button).click()
-                return True
-        return False
+    def get_post_time(self):
+        if len(self.__metadata) == 0:
+            return -1
 
-    def previous_button(self):
-        if len(self.__browser.page_source) == 0:
-            return False
-
-        soup = BeautifulSoup(self.__browser.page_source, 'lxml')
-        results = soup.find_all('div')
-        for result in results:
-            r = re.search("([_\d\w]+)\s+([_\d\w]+)\s" + PHOTO_PREV_BUTTON, str(result))
-            if r:
-                next_button = 'a.' + r.group(1) + '.' + r.group(2) + '.' + PHOTO_PREV_BUTTON
-                self.__browser.find_element_by_css_selector(next_button)
-                return True
-        return False
+        return int(self.__metadata['entry_data']['PostPage'][0]['graphql']['shortcode_media']['taken_at_timestamp'])
 
 
 def main():
-    album = AlbumHandler(ALBUM_URL, USER)
-    album.get_reply_count()
+    album = AlbumHandler2('https://www.instagram.com/p/BjaMA2GFAty/?taken-by=annehathaway', 'annehathaway')
+    album.run()
+    print('status: ' + str(album.status_code))
+    print(album.get_photos())
+    print('comment count: ' + str(album.get_comment_count()))
+    print('like count: ' + str(album.get_like_count()))
+    print('timestamp: ' + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(album.get_post_time()))))
 
 
 if __name__ == '__main__':
