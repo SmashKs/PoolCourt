e5fa730294645eb028ba0c46bebc3214d69faa2e
Weian Cheng <weian_cheng@htc.com>
2018-06-12 10:55:23 +0800

    In Firebase, if the same directory has been updated twice. The second time, original record would be deleted.
    The workaround is that all of record store in the buffer, and then write up to Firebase.

--- instagram/Instagram.py
+++ instagram/Instagram.py
@@ -1,3 +1,5 @@
+import datetime
+
 from instagram.InstagramRequest import InstagramRequest
 from instagram.MainPageParser import MainPageParser
 from instagram.AlbumHandler import AlbumHandler
@@ -23,13 +25,23 @@ class Instagram:
     def get_photos(self, user_id):
         main_page = MainPageParser(user_id, self.__request)
         albums = main_page.run()
+        images = {}
         for album in albums:
             self.__album_handler.run(album)
             title = self.__album_handler.get_short_code()
+            photo_list = []
             photos = self.__album_handler.get_photos()
+            for photo in photos:
+                photo_list.append(photos[photo])
             tags = self.__album_handler.get_tags()
             likes = self.__album_handler.get_like_count()
             author = self.__album_handler.get_author()
-            post_date = self.__album_handler.get_post_time()
-            img = ImageDetailObj(title=title, uri_list=photos, tag_list=tags, likes=likes, author=author, date=post_date)
-            self.__firebase.write_image_properties(dict(ImageDataObj(self.__album_handler.get_short_code(), img)))
+            post_date = datetime.datetime.fromtimestamp(self.__album_handler.get_post_time())
+            img = ImageDetailObj(title=title,
+                                 uri_list=photo_list,
+                                 tag_list=tags,
+                                 likes=likes,
+                                 author=author,
+                                 date=post_date)
+            images.update(ImageDataObj(self.__album_handler.get_short_code(), img))
+        self.__firebase.write_image_properties(images)
